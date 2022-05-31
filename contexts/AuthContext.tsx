07372a9895178f7a5f3169a68c65338780e66de0
glaubermatos <glaub.oliveira@hotmail.com>
2022-05-31 16:48:53 -0300

    performing refresh token

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -56,7 +56,7 @@ export function AuthProvider({children}: AuthProviderProps) {
                 maxAge: 60 * 60 * 24 * 30, //30 days
                 path: '/'
             })
-            setCookie(undefined, 'nextauth.refreshTooken', refreshToken, {
+            setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
                 maxAge: 60 * 60 * 24 * 30, //30 days
                 path: '/'
             })
--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from "axios";
+import { setCookie, parseCookies } from 'nookies'
 
-const cookies = parseCookies()
+let cookies = parseCookies()
 
 export const api = axios.create({
     baseURL: 'http://localhost:3333',
@@ -11,3 +11,38 @@ export const api = axios.create({
 })
 
 api.defaults.headers.common['Authorization'] = `Bearer ${cookies['nextauth.token']}`
+
+api.interceptors.response.use(response => {
+    return response
+}, (error) => {
+    if (error.response?.status === 401) {        
+        if (error.response.data.code === 'token.expired') {
+            //renovar o token
+            cookies = parseCookies()
+
+            const { 'nextauth.refreshToken': refreshToken } = cookies
+
+            api.post('/refresh', {
+                refreshToken
+            }).then(response => {
+                const { token } = response.data
+                console.log(token)
+
+                setCookie(undefined, 'nextauth.token', token, {
+                    maxAge: 60 * 60 * 24 * 30, // 30 days
+                    path: '/'
+                })
+
+                setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+                    maxAge: 60 * 60 * 24 * 30, // 30 days
+                    path: '/'
+                })                
+
+                api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+            })
+
+        } else {
+            // deslogar o usuário
+        }
+    }
+})
