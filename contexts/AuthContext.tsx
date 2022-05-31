de81ced3ff892180ac8b820bb2cbab9cdcd7e6fa
glaubermatos <glaub.oliveira@hotmail.com>
2022-05-31 07:04:38 -0300

    saving user token in cookies with nookies lib

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { setCookie } from 'nookies'
 import { createContext, ReactNode, useContext, useState } from "react";
 import { api } from "../services/api";
 
@@ -38,7 +39,16 @@ export function AuthProvider({children}: AuthProviderProps) {
                 password
             })
             
-            const { permissions, roles } = response.data
+            const { token, refreshToken, permissions, roles } = response.data
+
+            setCookie(undefined, 'nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, //30 days
+                path: '/'
+            })
+            setCookie(undefined, 'nextauth.refreshTooken', refreshToken, {
+                maxAge: 60 * 60 * 24 * 30, //30 days
+                path: '/'
+            })
 
             setUser({
                 email,
